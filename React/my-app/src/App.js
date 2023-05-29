db908d22d201d942a81dfa20ae0cd456d185bc5d
Nzioka6350 <jamesnziokamutua1@gmail.com>
2023-05-29 11:37:49 +0300

    React

---
+++ React/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './Components/Greet';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet/>  <Greet/>  <Greet/>  <Greet/>
+    </div>
+  );
+}
+
+export default App;
